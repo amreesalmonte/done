4242a79f407e78ac21a1b3cffd5652d5a09b27f3
amreesalmonte <amreesalmonte@gmail.com>
2020-01-21 15:53:19 -0700

    added route to settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:done/theme.dart';
 import 'package:done/body.dart';
+import 'package:done/settings.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,31 +24,37 @@ class _Home extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mainColor,
+      // constant app bar on top with settings button
+      appBar: AppBar(
         backgroundColor: mainColor,
-        // constant app bar on top with settings button
-        appBar: AppBar(
-          backgroundColor: mainColor,
-          elevation: 0,
-          actions: <Widget>[
-            //redirects to settings
-            IconButton(
-              icon: Icon(Icons.settings, color: secondaryColor),
-              tooltip: 'Settings',
-              onPressed: null,
-            )
-          ],
-        ),
-        // contains welcome message and items
-        body: Body(),
-        // button to add to do item
-        floatingActionButton: FloatingActionButton(
-          onPressed: () { print('Clicked'); },
+        elevation: 0,
+        actions: <Widget>[
+          //redirects to settings
+          IconButton(
+            icon: Icon(Icons.settings, color: secondaryColor),
+            tooltip: 'Settings',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Settings()),
+              );
+            },
+          )
+        ],
+      ),
+      // contains welcome message and items
+      body: Body(),
+      // button to add to do item
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Clicked');
+          },
           elevation: 0,
           splashColor: accentColor,
           tooltip: "Add item to list",
           backgroundColor: secondaryColor,
-          child: Icon(Icons.add, color: mainColor)
-        ),
-      );
+          child: Icon(Icons.add, color: mainColor)),
+    );
   }
 }
