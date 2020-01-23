073f143ec80d4264bff041b0536989144fde8615
amreesalmonte <amreesalmonte@gmail.com>
2020-01-23 15:57:15 -0700

    added lists

--- lib/body.dart
+++ lib/body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:done/theme.dart';
-import 'package:done/makeList.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,30 +7,34 @@ class Body extends StatefulWidget {
 }
 
 class _Body extends State<Body> {
+  List<String> items = [];
+  bool checked = false;
+
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        // contains welcome message
-        SliverAppBar(
-          backgroundColor: mainColor,
-          expandedHeight: 300.0,
-          flexibleSpace: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 30),
-                child: Text("Hello.", style: TextStyle(color: header, fontSize: 36)),
+    return Container(
+        child: ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return Container(
+          margin: EdgeInsets.only(left: 25, right: 25, bottom: 2, top: 2),
+          child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 30),
-                child: Text("Here's what needs to be done.", style: TextStyle(color: header, fontSize: 18)),
-              ),
-        SliverList(delegate: ToDoList(),)
-          ],)
-        )
-      ],
-    );
+              color: secondaryColor,
+              child: Row(children: <Widget>[
+                Checkbox(
+                  checkColor: secondaryColor,
+                  activeColor: mainColor,
+                  value: checked,
+                  onChanged: (value) {
+                    print("VALUE : $checked");
+                    setState(() {
+                      checked = value;
+                    });
+                  },
+                ),
+                Text("test")
+              ])));
+    }));
   }
 }
